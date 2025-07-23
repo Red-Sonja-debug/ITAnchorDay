188dddbe43c2d1be52bb072ee59bb749b9625e0f
Red-Sonja-debug <red.sonja.dnd@gmail.com>
2025-07-23 17:52:15 +0100

    Update Hello.py

--- Hello.py
+++ Hello.py
@@ -1,2 +1,6 @@
 # Always check the comments
 # You never know what you may find here. 
+#
+# Maybe even the next breadcrumb
+# You're looking for a Twitter/X profile
+# It's made up of this characters name, followed by the year the character was made for Marvel comics 
